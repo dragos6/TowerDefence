08e3de07e84680dfe1b332a138c3911aacbb48b9
dragos6 <99660191+dragos6@users.noreply.github.com>
2022-10-03 00:43:46 +0300

    Raycasts

--- TowerDefence/Assets/Scripts/TowerManager.cs
+++ TowerDefence/Assets/Scripts/TowerManager.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class TowerManager : MonoBehaviour
@@ -9,17 +8,33 @@ public class TowerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+            if(hit.collider.tag == "BuildSite")
+            PlaceTower(hit);
+
+        }
     }
 
-    public void SelectedTower (TowerBtn towerSelected)
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && towerBtnPressed != null)
         {
+            GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
+            newTower.transform.position = hit.transform.position;
+        }
+    }
+
+    public void SelectedTower(TowerBtn towerSelected)
+    {
         towerBtnPressed = towerSelected;
         Debug.Log("Pressed " + towerBtnPressed.gameObject);
     }
